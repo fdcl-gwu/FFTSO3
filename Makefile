19f0c1111154c165f3b64e93c64045fccc17d0b4
tylee <tylee@gwu.edu>
2018-07-17 14:30:39 -0400

    FFTSO3 splitted into real and complex

--- Makefile
+++ Makefile
@@ -4,14 +4,17 @@ VRPN_LIBS = -lvrpn -lquat -pthread
 OPTI_FLAG = -O3
 GTK_LIBS = -rdynamic `pkg-config --cflags gtk+-3.0`  `pkg-config --libs gtk+-3.0` 
 
-test_FFTSO3: test_FFTSO3.o fdcl_FFTSO3.o fdcl_FFTSO3_matrix.o fdcl_tictoc.o misc_matrix_func.o 
-	g++ -o test_FFTSO3  test_FFTSO3.o fdcl_FFTSO3.o fdcl_FFTSO3_matrix.o fdcl_tictoc.o misc_matrix_func.o -lm $(CFLAGS) $(OPTI_FLAGS)
+test_FFTSO3: test_FFTSO3.o fdcl_FFTSO3_real.o fdcl_FFTSO3_complex.o fdcl_FFTSO3_matrix.o fdcl_tictoc.o misc_matrix_func.o 
+	g++ -o test_FFTSO3  test_FFTSO3.o fdcl_FFTSO3_real.o fdcl_FFTSO3_complex.o fdcl_FFTSO3_matrix.o fdcl_tictoc.o misc_matrix_func.o -lm $(CFLAGS) $(OPTI_FLAGS)
 
 test_FFTSO3.o: test_FFTSO3.cpp
 	g++ -c test_FFTSO3.cpp $(CFLAGS) $(OPTI_FLAGS)
 
-fdcl_FFTSO3.o: fdcl_FFTSO3.cpp fdcl_FFTSO3.hpp
-	g++ -c fdcl_FFTSO3.cpp $(CFLAGS) $(OPTI_FLAGS)
+fdcl_FFTSO3_real.o: fdcl_FFTSO3_real.cpp fdcl_FFTSO3_real.hpp
+	g++ -c fdcl_FFTSO3_real.cpp $(CFLAGS) $(OPTI_FLAGS)
+
+fdcl_FFTSO3_complex.o: fdcl_FFTSO3_complex.cpp fdcl_FFTSO3_complex.hpp
+	g++ -c fdcl_FFTSO3_complex.cpp $(CFLAGS) $(OPTI_FLAGS)
 
 fdcl_tictoc.o: fdcl_tictoc.cpp fdcl_tictoc.hpp
 	g++ -c fdcl_tictoc.cpp $(CFLAGS) $(OPTI_FLAGS)
--- test_FFTSO3.cpp
+++ test_FFTSO3.cpp
@@ -6,7 +6,8 @@
 
 
 #include "fdcl_FFTSO3_matrix.hpp"
-#include "fdcl_FFTSO3.hpp"
+#include "fdcl_FFTSO3_complex.hpp"
+#include "fdcl_FFTSO3_real.hpp"
 #include "fdcl_tictoc.hpp"
 #include "misc_matrix_func.h"
 
@@ -36,10 +37,11 @@ complex<double> myfR(Matrix3 R)
 
 int main()
 {
-    int l_max=4;
+    int l_max=2;
     fdcl_FFTSO3_matrix_real d(l_max), d1(l_max), F_real(l_max);
     fdcl_FFTSO3_matrix_complex D(l_max), F0(l_max), F1(l_max), F(l_max);
-    fdcl_FFTSO3 FFTSO3(l_max);
+    fdcl_FFTSO3_complex FFTSO3(l_max);
+    fdcl_FFTSO3_real FFTSO3_real(l_max);
     fdcl_tictoc tictoc;
     double a, b, g;
     a=.12345;
@@ -55,12 +57,12 @@ int main()
     R1=expm_SO3(eta1);
     R2=expm_SO3(eta2);
     
-    // FFTSO3.check_weight();
-    // FFTSO3.check_wigner_d();
-    // FFTSO3.check_wigner_D_real();
-    // FFTSO3.check_forward_transform();
-    // FFTSO3.check_forward_transform_real();
-    F_real=FFTSO3.forward_transform_real(myrfR);
-    cout << FFTSO3.inverse_transform_real(F_real,R1) << endl;
-    cout << myrfR(R1) << endl;
+    FFTSO3.check_weight();
+    FFTSO3.check_wigner_d();
+    FFTSO3.check_forward_transform();
+
+    FFTSO3_real.check_weight();
+    FFTSO3_real.check_wigner_d();
+    FFTSO3_real.check_wigner_D_real();
+    FFTSO3_real.check_forward_transform();
 }
