924f9a9ab180204ac94694584452102f25b2210f
tylee <tylee@gwu.edu>
2018-08-24 16:21:52 -0400

    testing openmp

--- scratch/test_openmp.cpp
+++ scratch/test_openmp.cpp
@@ -10,16 +10,16 @@ using namespace std;
 
 int main()
 {
-    int l_max=1000;
+    int l_max=100;
     double dt1, dt2;
     fdcl_tictoc tt;
     fdcl_FFTS2_complex FFTS2(l_max);
 
-    omp_set_dynamic(0);     // Explicitly disable dynamic teams
+    // omp_set_dynamic(0);     // Explicitly disable dynamic teams
 
     std::vector<double> dt;
     dt.resize(4);
-    for(int r=0; r<=3; r++)
+    for(int r=0; r<=2; r++)
     {
         omp_set_num_threads(std::pow(2,r)); // 
         tt.tic();
