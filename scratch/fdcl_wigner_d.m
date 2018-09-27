58d602a443f0b932311062b015b9da824e24cb23
tylee <tylee@gwu.edu>
2018-09-27 16:52:27 -0400

    removed scratch

--- scratch/fdcl_wigner_d.m
+++
@@ -1,8 +0,0 @@
-function D=fdcl_wigner_D(a,b,g)
-
-
-D=[(1+cos(b))/2*exp(i*(a+g)) 1/sqrt(2)*sin(b)*exp(i*a) (1-cos(b))/2*exp(i*(a-g));
-    -1/sqrt(2)*sin(b)*exp(i*g) cos(b) 1/sqrt(2)*sin(b)*exp(-i*g);
-    (1-cos(b))/2*exp(-i*(a-g)) -1/sqrt(2)*sin(b)*exp(-i*a) (1+cos(b))/2*exp(-i*(a+g))];
-
-end
