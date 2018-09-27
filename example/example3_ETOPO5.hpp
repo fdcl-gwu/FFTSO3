0b65418f1588a2593ccd1701f6f2f31a91657bfb
tylee <tylee@gwu.edu>
2018-09-27 17:10:46 -0400

    example3 hpp files

---
+++ example/example3_ETOPO5.hpp
@@ -0,0 +1,91 @@
+namespace fdcl
+{
+    class ETOPO5;
+}
+
+/** Access Earth Topological Data, namely ETOPO5 provided by NOAA at https://www.ngdc.noaa.gov/mgg/global/etopo5.HTML
+*/
+
+class fdcl::ETOPO5
+{
+    public:
+        ETOPO5(){};
+        ~ETOPO5(){};
+    
+        std::ifstream fd;
+        int N_lat, N_lon; /**< Number of grid for latitude and longitude */
+        Eigen::MatrixXi elev_data; /**< buffer */
+        Eigen::VectorXd lat, lon; /**< latitude and longitude at grid */
+        void init(std::string filename, int N_lat, int N_lon); /**< initialize variables */
+        void read(std::string filename, int N_lat, int N_lon); /** read data stored at file */
+        double elev(double lat, double lon); /**< compute elevation at the given latitude and longitude */
+        double operator()(double lat, double lon);  /**< operator overloading for elev() */
+    private: 
+        void unit_quorem(double num, double den, int& quo, double& rem);  /** compute quotient and remainder */
+};
+
+void fdcl::ETOPO5::init(std::string filename, int N_lat, int N_lon)
+{
+    this->N_lat=N_lat;
+    this->N_lon=N_lon;
+    fd.open(filename.c_str());
+    elev_data.resize(N_lat,N_lon);
+    lat.resize(N_lat);
+    lon.resize(N_lon);
+
+    for(int i=0; i<N_lon; i++)
+        lon(i)=((double)4320/N_lon)*((double)5/60)*(double)i;
+    for(int i=0; i<N_lat; i++)
+        lat(i)=((double)2160/N_lat)*((double)5/60)*(double)i;
+}
+
+void fdcl::ETOPO5::read(std::string filename, int N_lat, int N_lon)
+{
+    init(filename,N_lat,N_lon);
+    for(int i_lat=0; i_lat<N_lat; i_lat++)
+        for(int i_lon=0; i_lon<N_lon; i_lon++)
+            fd >> elev_data(i_lat,i_lon);
+}
+
+void fdcl::ETOPO5::unit_quorem(double num, double den, int& quo, double& rem)
+{
+    double ratio;
+    ratio=num/den;
+    
+    quo=std::floor(ratio);
+    rem=ratio-quo;
+}
+
+double fdcl::ETOPO5::elev(double lat, double lon)
+{
+    // bilinear interpolation with scaling
+    // https://en.wikipedia.org/wiki/Bilinear_interpolation
+    
+    assert(lat >= -90. && lat <=90.);
+    assert(lon >= 0. && lon <= 360.);
+
+    double x, y;
+    int i0, i1, j0, j1;
+
+    unit_quorem(-lat+90., (((double)2160/N_lat)*((double)5/60)), i0, x);
+    unit_quorem(lon, (((double)4320/N_lon)*((double)5/60)), j0, y);
+
+    if (i0==N_lat-1)
+        i1=0;
+    else
+        i1=i0+1;
+
+    if (j0==N_lon-1)
+        j1=0;
+    else
+        j1=j0+1;
+
+    return elev_data(i0,j0)*(1.-x)*(1.-y)+elev_data(i1,j0)*x*(1.-y)+elev_data(i0,j1)*(1.-x)*y+elev_data(i1,j1)*x*y;
+}
+
+double fdcl::ETOPO5::operator()(double lat, double lon)
+{
+    return elev(lat,lon);
+}
+
+
---
+++ example/example3_SSM.hpp
@@ -0,0 +1,119 @@
+namespace fdcl
+{
+    class spherical_shape_matching;
+}
+
+/** Class for Spherical Shape Matching with Harmonic Analysis 
+ */
+class fdcl::spherical_shape_matching
+{
+    public:
+        fdcl::FFTSO3_real RFFTSO3;
+
+        fdcl::FFTS2_matrix_real F, G; /**< Spherical harmonic for the original and the rotated shape/image */
+        fdcl::FFTSO3_matrix_real U; /**< Real harmonics to represent rotation */
+        std::vector<fdcl::FFTSO3_matrix_real> u; /**< Derivatives of real harmonics */
+        int l_max;
+        double scale_factor=1.e-6; /**< scale factor for cost and gradient */
+        double eps=1.e-6; /**< stopping threshold */
+        double step_size=1.e-3; /**< step size for update */
+
+        spherical_shape_matching(){};
+        ~spherical_shape_matching(){};
+        spherical_shape_matching(int l_max);
+
+        void init(int l_max); /**< Initialize variables */
+        double J(Eigen::Matrix3d R); /**< Cost function for a given rotation */
+        Eigen::Vector3d gradient(Eigen::Matrix3d R); /**< Gradient of cost */
+        Eigen::Matrix3d opt(Eigen::Matrix3d R0); /**< Steepest descent optimization */
+        void check_gradient(); /**< Verify gradient computation */
+};
+
+fdcl::spherical_shape_matching::spherical_shape_matching(int l_max)
+{
+    init(l_max);
+}
+
+void fdcl::spherical_shape_matching::init(int l_max)
+{
+    this->l_max = l_max;
+    F.init(l_max);
+    G.init(l_max);
+    RFFTSO3.init(l_max);
+    U.init(l_max);
+    u.resize(4);
+    u[1].init(l_max);
+    u[2].init(l_max);
+    u[3].init(l_max);   
+    u = RFFTSO3.deriv_real_harmonics();
+}
+
+double fdcl::spherical_shape_matching::J(Eigen::Matrix3d R)
+{
+    double y=0.;
+    U = RFFTSO3.real_harmonics(R);
+    for(int l=0; l<=l_max; l++)
+        y+=(G[l]*F[l].transpose()*U[l]).trace();
+
+    return y*scale_factor;
+}
+
+Eigen::Vector3d fdcl::spherical_shape_matching::gradient(Eigen::Matrix3d R)
+{
+    Eigen::Vector3d dJ;
+    dJ.setZero();
+    U = RFFTSO3.real_harmonics(R);
+
+    for(int l=0; l<=l_max; l++)
+        for(int i=1; i<=3; i++)
+            dJ(i-1)+=(G[l]*F[l].transpose()*U[l]*u[i][l]).trace();
+
+    return dJ*scale_factor;
+}
+
+Eigen::Matrix3d fdcl::spherical_shape_matching::opt(Eigen::Matrix3d R0)
+{
+    Eigen::Matrix3d R;
+    Eigen::Vector3d eta;
+    double norm_gradient=1.;
+    int i_iter=0;
+    R=R0;
+    std::vector<double> abg;
+
+    while(norm_gradient > eps)
+    {
+
+        eta=gradient(R);
+        R=R*expm_SO3(step_size*eta);
+        norm_gradient=eta.norm();
+        i_iter+=1;
+
+        abg=R2Euler323(R);
+
+        cout << "iter = " << i_iter << " : J = " << J(R) << " : n_grad = " << norm_gradient << " : Euler = " << abg[0] << " " << abg[1] << " " << abg[2] << endl;
+    }
+
+    return R;
+}
+void fdcl::spherical_shape_matching::check_gradient()
+{
+    Eigen::Matrix3d R, R_new;
+    Eigen::Vector3d eta;
+    double a,b,g;
+
+    a=(double)rand()/RAND_MAX*2.*M_PI;
+    b=(double)rand()/RAND_MAX*M_PI;
+    g=(double)rand()/RAND_MAX*2.*M_PI;
+
+    R=Euler3232R(a,b,g);
+    eta.setRandom();
+    eta*=1.e-6;
+
+    R_new=R*expm_SO3(eta);
+
+    gradient(R);
+    cout << J(R_new)-J(R) << endl;
+    cout << gradient(R).transpose()*eta << endl;
+}
+
+
